d95ac89bb9087a141b28f6e0b9da207ff62fc8f9
wh0am1 <walkonearth@qq.com>
2017-07-26 20:08:50 +0800

    fix SplitVideoToFrames

--- SplitVideoToFrames.py
+++ SplitVideoToFrames.py
@@ -8,15 +8,18 @@ while True:
     # Capture frame-by-frame
     ret, frame = cap.read()
 
+    face_cascade = cv2.CascadeClassifier('opencv_config/haarcascade_frontalface_default.xml')
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    cv2.imwrite("./image_filter/frame%d.jpg" % count, frame)     # save frame as JPEG file
-    count += 1
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    if len(faces)> 0:
+        cv2.imwrite("./image_filter/frame%d.jpg" % count, frame)     # save frame as JPEG file
+        count += 1
 
     # Display the resulting frame
     #cv2.imshow('frame',gray)
-    if count > 1:
+    if count > 100:
         cv2.destroyAllWindows()
         break
 
