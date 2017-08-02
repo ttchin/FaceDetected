36a1e20e3d4a68ebcc9016225048b78ed90dd0e7
Chao Qin <chao.qin@ericsson.com>
2017-08-02 18:14:43 +0800

    simplify model training, drop result if prob < 0.95

--- DetectFaces.py
+++ DetectFaces.py
@@ -32,7 +32,7 @@ def detect_faces_from_picture(pic_file_path):
 
 def detect_faces_from_camera_video_stream(exec_time=60):
     # Perform the detection every n seconds
-    detect_interval = 0.6
+    detect_interval = 1
 
     cap = cv2.VideoCapture(0)
     exec_start = time.time()
@@ -60,7 +60,7 @@ def detect_faces_from_camera_video_stream(exec_time=60):
             #faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.2, minNeighbors=3, minSize=(10, 10))
             
             # Match the detected faces with the trained model
-            if len(faces) > 0:
+            if len(faces) == 1:
                 print(">>> Someone is out there!")
                 for (x, y, w, h) in faces:
                     face = frame[y:y+h, x:x+w]
@@ -68,13 +68,16 @@ def detect_faces_from_camera_video_stream(exec_time=60):
                     # Draw rectangles which point out the faces
                     cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-                    result = model.predict(face)
-                    for index, name in model.getTrainCfg():
-                        if result == index:
-                            print(">>> Aha, it's %s!" % name)
-                            detected_name = name
-                            break
-
+                    label,probe = model.predict(face)
+                    if probe > 0.95:
+                        for index, name in model.getTrainCfg():
+                            if label == index:
+                                print(">>> Aha, it's %s!" % name)
+                                detected_name = name
+                                break
+            elif len(faces) > 1:
+                print("Too many people here, I am going to die!")
+                
         # Display the camero video
         cv2.putText(frame, "Press 'q' to quit", (20, 40),
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
